c2d95d15abbece81a8d0534ede749a434a2d30b8
CedricFauth <cedricfauth@yahoo.de>
2020-09-19 10:16:56 +0000

    bootloader v0.1

--- v0.1/src/bootloader.asm
+++ v0.1/src/bootloader.asm
@@ -6,32 +6,37 @@
 ; setting up directives for nasm
 
 bits 16				; only 16 bit support
-org 0x7c00			; base adress in memory
+org 0x0000			; base adress in memory
 
 
-jmp 0x00:entry			; far jump into entry point
-				; ... to setup cs properly
+jmp 0x7c0:entry			; far jump to entry (0x7c0 * 0x10 + entry)
+				; ... 0x7c00 to setup cs properly
 ; boot loader's entry point
 
 entry:
- 
-    cli
-    xor ax, ax			; ax = 0
-    mov ds, ax			; data segment = base + 0
-    mov ss, ax			; stack segment = base + 0
-    mov es, ax			; extra segment = base + 0
 
-    mov bp, 0x7c00		; set base pointer under boot code
-    mov sp, bp			; set stack pointer = base pointer
-    sti
+    mov bp, 0x7c00		; set base pointer to 0x7c00
+    mov ax, 0x7c0		; store 0x7c0 temporarily
+    mov ds, ax			; data segment: 0x7c0 (*0x10 when addressing)
+    mov es, ax			; extra segment: 0x7c0 (*0x10 when addressing)
+
+    xor ax,ax			; store 0 temporarily
+    mov ss, ax			; set ss base to 0x0
+    mov sp, bp			; set stack pointer to 0x7c00
 
     ; print letter A via BIOS int
 
-    push 'A'
-    mov ah, 0x0e
-    pop bx
+    mov ah, 0x0e		; BIOS teletype output
+    push 'B'			; push 'B' on stack
+    push 'A'			; push 'A' on stack
+
+    pop bx			; pop 'A'
+    mov al, bl			; move ASCII value from BL to AL
+    int 0x10			; raise interrupt: BIOS print routine
+
+    mov bx, [bp-2]		; mov 'A' from stack to BX
     mov al, bl
-    int 10h
+    int 0x10
 
     JMP $			; endless loop
 
